eacf9ad847ca8990b5bef9ac6ebcb3f521f41c5b
StayZeal <543294760@qq.com>
2019-10-29 15:26:54 +0800

    str

--- push.sh
+++ push.sh
@@ -3,10 +3,10 @@ git add .
 commit(){
   str = "$1"
   if [ -z "$str" ]; then
-    str = "default commit msg"
-  fi
-   git commit -m str
-echo str
+     str = "default commit msg"
+  fi    
+  git commit -m str
+  echo str
 }
 commit
 git push
